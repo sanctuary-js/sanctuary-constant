c1474b4879da3a6fad34c32113864f58dc5ea3c1
David Chambers <dc@davidchambers.me>
2019-03-01 12:05:37 +0100

    everything

---
+++ index.js
@@ -0,0 +1,330 @@
+/*   _____   _____   _____   _____   ______   _____   _____   ______   */
+/*  |     | |     | |     | |     | |      | |     | |     | |      |  */
+/*  |  |__| |  |  | |  |  | |  |__| '-,  ,-' |  |  | |  |  | '-,  ,-'  */
+/*  |  |__  |  |  | |  |  | |__   |   |  |   |     | |  |  |   |  |    */
+/*  |  |  | |  |  | |  |  | |  |  |   |  |   |  |  | |  |  |   |  |    */
+/*  |_____| |_____| |__|__| |_____|   |__|   |__|__| |__|__|   |__|    */
+
+//. <a href="https://github.com/fantasyland/fantasy-land"><img alt="Fantasy Land" src="https://raw.githubusercontent.com/fantasyland/fantasy-land/master/logo.png" width="75" height="75" align="left"></a>
+//.
+//. # sanctuary-constant
+//.
+//. A value of type `Constant a b` always contains exactly one value,
+//. of type `a`. Mapping over a `Constant a b` has no effect because
+//. the `b -> c` function is never applied.
+
+(function(f) {
+
+  'use strict';
+
+  var util = {inspect: {}};
+
+  /* istanbul ignore else */
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = f (require ('util'),
+                        require ('sanctuary-show'),
+                        require ('sanctuary-type-classes'));
+  } else if (typeof define === 'function' && define.amd != null) {
+    define (['sanctuary-show', 'sanctuary-type-classes'], function(show, Z) {
+      return f (util, show, Z);
+    });
+  } else {
+    self.sanctuaryConstant = f (util,
+                                self.sanctuaryShow,
+                                self.sanctuaryTypeClasses);
+  }
+
+} (function(util, show, Z) {
+
+  'use strict';
+
+  /* istanbul ignore if */
+  if (typeof __doctest !== 'undefined') {
+    var $ = __doctest.require ('sanctuary-def');
+    var type = __doctest.require ('sanctuary-type-identifiers');
+    var S = (function() {
+      var S = __doctest.require ('sanctuary');
+      var typeIdent = (Constant (Array))['@@type'];
+      var ConstantType = $.BinaryType
+        ('sanctuary-constant/Constant')
+        ('')
+        (function(x) { return type (x) === typeIdent; })
+        (function(c) { return [c.value]; })
+        (function(c) { return []; });
+      var env = Z.concat (
+        S.env,
+        [$.TypeClass, ConstantType ($.Unknown) ($.Unknown)]
+      );
+      return S.create ({checkTypes: true, env: env});
+    } ());
+  }
+
+  //. `Constant a b` satisfies the following [Fantasy Land][] specifications:
+  //.
+  //. ```javascript
+  //. > const Useless = require ('sanctuary-useless')
+  //.
+  //. > S.map (k =>
+  //. .   k + ' '.repeat (16 - k.length) +
+  //. .   (Z[k].test (Constant (Useless.constructor) (Useless)) ? '\u2705   ' :
+  //. .    Z[k].test (Constant (Array) (['foo', 'bar', 'baz'])) ? '\u2705 * ' :
+  //. .    /* otherwise */                                        '\u274C   ')
+  //. . ) (S.keys (Z.filter ($.test ([]) ($.TypeClass), Z)))
+  //. [ 'Setoid          ✅ * ',  // if ‘a’ satisfies Setoid
+  //. . 'Ord             ✅ * ',  // if ‘a’ satisfies Ord
+  //. . 'Semigroupoid    ❌   ',
+  //. . 'Category        ❌   ',
+  //. . 'Semigroup       ✅ * ',  // if ‘a’ satisfies Semigroup
+  //. . 'Monoid          ❌   ',
+  //. . 'Group           ❌   ',
+  //. . 'Filterable      ❌   ',
+  //. . 'Functor         ✅   ',
+  //. . 'Bifunctor       ✅   ',
+  //. . 'Profunctor      ❌   ',
+  //. . 'Apply           ✅ * ',  // if ‘a’ satisfies Semigroup
+  //. . 'Applicative     ✅ * ',  // if ‘a’ satisfies Monoid
+  //. . 'Chain           ❌   ',
+  //. . 'ChainRec        ❌   ',
+  //. . 'Monad           ❌   ',
+  //. . 'Alt             ❌   ',
+  //. . 'Plus            ❌   ',
+  //. . 'Alternative     ❌   ',
+  //. . 'Foldable        ✅   ',
+  //. . 'Traversable     ✅   ',
+  //. . 'Extend          ❌   ',
+  //. . 'Comonad         ❌   ',
+  //. . 'Contravariant   ❌   ' ]
+  //. ```
+
+  //# Constant :: TypeRep a -> a -> Constant a b
+  //.
+  //. Constant's sole data constructor. The [type representative][] makes
+  //. `Constant (M)` an [Applicative][]-compatible type representative if
+  //. `M` represents some monoidal type.
+  //.
+  //. ```javascript
+  //. > Constant (String) ('abc')
+  //. Constant (String) ('abc')
+  //.
+  //. > Constant (Number) (123)
+  //. Constant (Number) (123)
+  //. ```
+  function Constant(A) {
+    var prototype = {
+      /* eslint-disable key-spacing */
+      'constructor':            Constant$bound,
+      '@@show':                 Constant$prototype$show,
+      'fantasy-land/map':       Constant$prototype$map,
+      'fantasy-land/bimap':     Constant$prototype$bimap,
+      'fantasy-land/reduce':    Constant$prototype$reduce,
+      'fantasy-land/traverse':  Constant$prototype$traverse
+      /* eslint-enable key-spacing */
+    };
+
+    var custom = util.inspect.custom;
+    /* istanbul ignore else */
+    if (typeof custom === 'symbol') {
+      prototype[custom] = Constant$prototype$show;
+    } else {
+      prototype.inspect = Constant$prototype$show;
+    }
+
+    function Constant$bound(value) {
+      var identity = Object.create (prototype);
+      if (Z.Setoid.test (value)) {
+        identity['fantasy-land/equals'] = Constant$prototype$equals;
+        if (Z.Ord.test (value)) {
+          identity['fantasy-land/lte'] = Constant$prototype$lte;
+        }
+      }
+      if (Z.Semigroup.test (value)) {
+        identity['fantasy-land/concat'] = Constant$prototype$concat;
+        identity['fantasy-land/ap'] = Constant$prototype$ap;
+      }
+      identity.value = value;
+      return identity;
+    }
+
+    Constant$bound.toString = function() {
+      if (A['@@type'] === Constant$bound['@@type']) {
+        return 'Constant (' + show (A) + ')';
+      } else if (Object.prototype.hasOwnProperty.call (A, 'name')) {
+        return 'Constant (' + A.name + ')';
+      } else {
+        var source = String (A);
+        var match = /^\s*function ([$_A-Za-z][$_A-Za-z0-9]*)/.exec (source);
+        return 'Constant (' + (match == null ? source : match[1]) + ')';
+      }
+    };
+
+    //# Constant.@@type :: String
+    //.
+    //. Constant [type identifier][].
+    //.
+    //. ```javascript
+    //. > type (Constant (Number) (42))
+    //. 'sanctuary-constant/Constant@1'
+    //.
+    //. > type.parse (type (Constant (Number) (42)))
+    //. {namespace: 'sanctuary-constant', name: 'Constant', version: 1}
+    //. ```
+    Constant$bound['@@type'] = 'sanctuary-constant/Constant@1';
+
+    //# Constant.fantasy-land/of :: Monoid m => a -> Constant m a
+    //.
+    //. `of (Constant (M)) (x)` is equivalent to `Constant (M) (empty (M))`.
+    //.
+    //. ```javascript
+    //. > S.of (Constant (Array)) (42)
+    //. Constant (Array) ([])
+    //.
+    //. > S.of (Constant (String)) (42)
+    //. Constant (String) ('')
+    //. ```
+    (function() {
+      var empty;
+      try { empty = Z.empty (A); } catch (err) { return; }
+      Constant$bound['fantasy-land/of'] = function Constant$of(x) {
+        return Constant$bound (empty);
+      };
+    } ());
+
+    //# Constant#@@show :: Showable a => Constant a b ~> () -> String
+    //.
+    //. `show (Constant (A) (x))` is equivalent to
+    //. `'Constant (' + A.name + ') (' + show (x) + ')'`.
+    //.
+    //. ```javascript
+    //. > show (Constant (Array) (['foo', 'bar', 'baz']))
+    //. 'Constant (Array) (["foo", "bar", "baz"])'
+    //. ```
+    function Constant$prototype$show() {
+      return String (Constant$bound) + ' (' + show (this.value) + ')';
+    }
+
+    //# Constant#fantasy-land/equals :: Setoid a => Constant a b ~> Constant a b -> Boolean
+    //.
+    //. `Constant (A) (x)` is equal to `Constant (A) (y)` [iff][] `x` is
+    //. equal to `y` according to [`Z.equals`][].
+    //.
+    //. ```javascript
+    //. > S.equals (Constant (Array) ([1, 2, 3]))
+    //. .          (Constant (Array) ([1, 2, 3]))
+    //. true
+    //.
+    //. > S.equals (Constant (Array) ([1, 2, 3]))
+    //. .          (Constant (Array) ([3, 2, 1]))
+    //. false
+    //. ```
+    function Constant$prototype$equals(other) {
+      return Z.equals (this.value, other.value);
+    }
+
+    //# Constant#fantasy-land/lte :: Ord a => Constant a b ~> Constant a b -> Boolean
+    //.
+    //. `Constant (A) (x)` is less than or equal to `Constant (A) (y)` [iff][]
+    //. `x` is less than or equal to `y` according to [`Z.lte`][].
+    //.
+    //. ```javascript
+    //. > S.filter (S.lte (Constant (Number) (1)))
+    //. .          ([Constant (Number) (0),
+    //. .            Constant (Number) (1),
+    //. .            Constant (Number) (2)])
+    //. [Constant (Number) (0), Constant (Number) (1)]
+    //. ```
+    function Constant$prototype$lte(other) {
+      return Z.lte (this.value, other.value);
+    }
+
+    //# Constant#fantasy-land/concat :: Semigroup a => Constant a b ~> Constant a b -> Constant a b
+    //.
+    //. `concat (Constant (A) (x)) (Constant (A) (y))` is equivalent to
+    //. `Constant (A) (concat (x) (y))`.
+    //.
+    //. ```javascript
+    //. > S.concat (Constant (Array) ([1, 2, 3]))
+    //. .          (Constant (Array) ([4, 5, 6]))
+    //. Constant (Array) ([1, 2, 3, 4, 5, 6])
+    //. ```
+    function Constant$prototype$concat(other) {
+      return Constant$bound (Z.concat (this.value, other.value));
+    }
+
+    //# Constant#fantasy-land/map :: Constant a b ~> (b -> c) -> Constant a c
+    //.
+    //. `map (f) (Constant (A) (x))` is equivalent to `Constant (A) (x)`.
+    //.
+    //. ```javascript
+    //. > S.map (Math.sqrt) (Constant (Number) (64))
+    //. Constant (Number) (64)
+    //. ```
+    function Constant$prototype$map(f) {
+      return this;
+    }
+
+    //# Constant#fantasy-land/bimap :: Constant a c ~> (a -> b, c -> d) -> Constant b d
+    //.
+    //. `bimap (f) (g) (Constant (A) (x))` is equivalent to
+    //. `Constant (A) (f (x))`.
+    //.
+    //. ```javascript
+    //. > S.bimap (s => s.length) (Math.sqrt) (Constant (String) ('abc'))
+    //. Constant (Number) (3)
+    //. ```
+    function Constant$prototype$bimap(f, g) {
+      var x = f (this.value);
+      return Constant (x.constructor) (x);
+    }
+
+    //# Constant#fantasy-land/ap :: Semigroup a => Constant a b ~> Constant a (b -> c) -> Constant a c
+    //.
+    //. `ap (Constant (A) (x)) (Constant (A) (y))` is equivalent to
+    //. `concat (Constant (A) (x)) (Constant (A) (y))`.
+    //.
+    //. ```javascript
+    //. > S.ap (Constant (Array) ([1, 2, 3])) (Constant (Array) ([4, 5, 6]))
+    //. Constant (Array) ([1, 2, 3, 4, 5, 6])
+    //. ```
+    function Constant$prototype$ap(other) {
+      return Z.concat (other, this);
+    }
+
+    //# Constant#fantasy-land/reduce :: Constant a b ~> ((c, b) -> c, c) -> c
+    //.
+    //. `reduce (f) (x) (Constant (A) (y))` is equivalent to `x`.
+    //.
+    //. ```javascript
+    //. > S.reduce (S.add) (100) (Constant (Number) (42))
+    //. 100
+    //. ```
+    function Constant$prototype$reduce(f, x) {
+      return x;
+    }
+
+    //# Constant#fantasy-land/traverse :: Applicative f => Constant a b ~> (TypeRep f, b -> f c) -> f (Constant a c)
+    //.
+    //. `traverse (A) (f) (Constant (X) (x))` is equivalent to
+    //. `of (A) (Constant (X) (x))`.
+    //.
+    //. ```javascript
+    //. > S.traverse (Array) (Math.sqrt) (Constant (Number) (64))
+    //. [Constant (Number) (64)]
+    //. ```
+    function Constant$prototype$traverse(typeRep, f) {
+      return Z.of (typeRep, this);
+    }
+
+    return Constant$bound;
+  }
+
+  return Constant;
+
+}));
+
+//. [Applicative]:              v:fantasyland/fantasy-land#Applicative
+//. [Fantasy Land]:             v:fantasyland/fantasy-land
+//. [`Z.equals`]:               v:sanctuary-js/sanctuary-type-classes#equals
+//. [`Z.lte`]:                  v:sanctuary-js/sanctuary-type-classes#lte
+//. [iff]:                      https://en.wikipedia.org/wiki/If_and_only_if
+//. [type identifier]:          v:sanctuary-js/sanctuary-type-identifiers
+//. [type representative]:      v:fantasyland/fantasy-land#type-representatives
---
+++ test/index.js
@@ -0,0 +1,353 @@
+'use strict';
+
+const assert        = require ('assert');
+
+const laws          = require ('fantasy-laws');
+const jsc           = require ('jsverify');
+const Identity      = require ('sanctuary-identity');
+const show          = require ('sanctuary-show');
+const Z             = require ('sanctuary-type-classes');
+const type          = require ('sanctuary-type-identifiers');
+const Useless       = require ('sanctuary-useless');
+
+const Constant      = require ('..');
+
+
+//    ConstantArb :: Monoid m => TypeRep m -> Arbitrary a -> Arbitrary (Constant a b)
+const ConstantArb = M => arb => arb.smap (Constant (M), c => c.value, show);
+
+//    IdentityArb :: Arbitrary a -> Arbitrary (Identity a)
+const IdentityArb = arb => arb.smap (Identity, Z.extract, show);
+
+//    NonEmpty :: Arbitrary a -> Arbitrary (NonEmpty a)
+const NonEmpty = arb => jsc.suchthat (arb, x => not (empty (x)));
+
+//    NumberArb :: Arbitrary Number
+const NumberArb = jsc.oneof (
+  jsc.constant (NaN),
+  jsc.constant (-Infinity),
+  jsc.constant (Number.MIN_SAFE_INTEGER),
+  jsc.constant (-10000),
+  jsc.constant (-9999),
+  jsc.constant (-0.5),
+  jsc.constant (-0),
+  jsc.constant (0),
+  jsc.constant (0.5),
+  jsc.constant (9999),
+  jsc.constant (10000),
+  jsc.constant (Number.MAX_SAFE_INTEGER),
+  jsc.constant (Infinity)
+);
+
+//    empty :: Monoid m => m -> Boolean
+const empty = m => Z.equals (m, Z.empty (m.constructor));
+
+//    not :: Boolean -> Boolean
+const not = b => !b;
+
+//    testLaws :: Object -> Object -> Undefined
+const testLaws = laws => arbs => {
+  (Object.keys (laws)).forEach (name => {
+    eq (laws[name].length) (arbs[name].length);
+    test (name.replace (/[A-Z]/g, c => ' ' + c.toLowerCase ()),
+          laws[name] (...arbs[name]));
+  });
+};
+
+//    eq :: a -> b -> Undefined !
+function eq(actual) {
+  assert.strictEqual (arguments.length, eq.length);
+  return function eq$1(expected) {
+    assert.strictEqual (arguments.length, eq$1.length);
+    assert.strictEqual (show (actual), show (expected));
+    assert.strictEqual (Z.equals (actual, expected), true);
+  };
+}
+
+
+suite ('Constant', () => {
+
+  test ('metadata', () => {
+    eq (typeof Constant) ('function');
+    eq (Constant.name) ('Constant');
+    eq (Constant.length) (1);
+  });
+
+  test ('@@type', () => {
+    eq (type (Constant (Number) (0))) ('sanctuary-constant/Constant@1');
+    eq (type.parse (type (Constant (Number) (0))))
+       ({namespace: 'sanctuary-constant', name: 'Constant', version: 1});
+  });
+
+  test ('@@show', () => {
+    eq (show (Constant (Array) (['foo', 'bar', 'baz'])))
+       ('Constant (Array) (["foo", "bar", "baz"])');
+    eq (show (Constant (Constant (Constant (Number)))
+                       (Constant (Constant (Number))
+                                 (Constant (Number)
+                                           (-0)))))
+       ('Constant (Constant (Constant (Number)))' +
+                ' (Constant (Constant (Number))' +
+                          ' (Constant (Number)' +
+                                    ' (-0)))');
+
+    //    Foo :: String -> Foo
+    function Foo(value) {
+      return {
+        'constructor': Foo,
+        '@@show': function Foo$show() { return 'Foo (' + show (value) + ')'; },
+      };
+    }
+    delete Foo.name;
+    eq (show (Constant (Foo) (Foo ('foo'))))
+       ('Constant (Foo) (Foo ("foo"))');
+
+    //    Bar :: String -> Bar
+    const Bar = value => ({
+      'constructor': Bar,
+      '@@show': function Bar$show() { return 'Bar (' + show (value) + ')'; },
+    });
+    delete Bar.name;
+    eq (show (Constant (Bar) (Bar ('bar'))))
+       ('Constant (' + String (Bar) + ') (Bar ("bar"))');
+  });
+
+});
+
+suite ('type-class predicates', () => {
+
+  test ('Setoid', () => {
+    eq (Z.Setoid.test (Constant (Useless.constructor) (Useless))) (false);
+    eq (Z.Setoid.test (Constant (RegExp) (/(?:)/))) (true);
+  });
+
+  test ('Ord', () => {
+    eq (Z.Ord.test (Constant (Useless.constructor) (Useless))) (false);
+    eq (Z.Ord.test (Constant (RegExp) (/(?:)/))) (false);
+    eq (Z.Ord.test (Constant (Number) (0))) (true);
+  });
+
+  test ('Semigroupoid', () => {
+    eq (Z.Semigroupoid.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Category', () => {
+    eq (Z.Category.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Semigroup', () => {
+    eq (Z.Semigroup.test (Constant (Useless.constructor) (Useless))) (false);
+    eq (Z.Semigroup.test (Constant (Number) (0))) (false);
+    eq (Z.Semigroup.test (Constant (Array) ([]))) (true);
+  });
+
+  test ('Monoid', () => {
+    eq (Z.Monoid.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Group', () => {
+    eq (Z.Group.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Filterable', () => {
+    eq (Z.Filterable.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Functor', () => {
+    eq (Z.Functor.test (Constant (Useless.constructor) (Useless))) (true);
+  });
+
+  test ('Bifunctor', () => {
+    eq (Z.Bifunctor.test (Constant (Useless.constructor) (Useless))) (true);
+  });
+
+  test ('Profunctor', () => {
+    eq (Z.Profunctor.test (Constant (Function) (Math.sqrt))) (false);
+  });
+
+  test ('Apply', () => {
+    eq (Z.Apply.test (Constant (Useless.constructor) (Useless))) (false);
+    eq (Z.Apply.test (Constant (Number) (0))) (false);
+    eq (Z.Apply.test (Constant (Array) ([]))) (true);
+  });
+
+  test ('Applicative', () => {
+    eq (Z.Applicative.test (Constant (Useless.constructor) (Useless))) (false);
+    eq (Z.Applicative.test (Constant (Number) (0))) (false);
+    eq (Z.Applicative.test (Constant (Array) ([]))) (true);
+  });
+
+  test ('Chain', () => {
+    eq (Z.Chain.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('ChainRec', () => {
+    eq (Z.ChainRec.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Monad', () => {
+    eq (Z.Monad.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Alt', () => {
+    eq (Z.Alt.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Plus', () => {
+    eq (Z.Plus.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Alternative', () => {
+    eq (Z.Alternative.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Foldable', () => {
+    eq (Z.Foldable.test (Constant (Useless.constructor) (Useless))) (true);
+  });
+
+  test ('Traversable', () => {
+    eq (Z.Traversable.test (Constant (Useless.constructor) (Useless))) (true);
+  });
+
+  test ('Extend', () => {
+    eq (Z.Extend.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Comonad', () => {
+    eq (Z.Comonad.test (Constant (Array) ([]))) (false);
+  });
+
+  test ('Contravariant', () => {
+    eq (Z.Contravariant.test (Constant (Function) (Math.sqrt))) (false);
+  });
+
+});
+
+suite ('Setoid laws', () => {
+  testLaws (laws.Setoid) ({
+    reflexivity: [
+      ConstantArb (Number) (NumberArb),
+    ],
+    symmetry: [
+      ConstantArb (Number) (NumberArb),
+      ConstantArb (Number) (NumberArb),
+    ],
+    transitivity: [
+      ConstantArb (Number) (NumberArb),
+      ConstantArb (Number) (NumberArb),
+      ConstantArb (Number) (NumberArb),
+    ],
+  });
+});
+
+suite ('Ord laws', () => {
+  testLaws (laws.Ord) ({
+    totality: [
+      ConstantArb (Number) (NumberArb),
+      ConstantArb (Number) (NumberArb),
+    ],
+    antisymmetry: [
+      ConstantArb (Number) (NumberArb),
+      ConstantArb (Number) (NumberArb),
+    ],
+    transitivity: [
+      ConstantArb (Number) (NumberArb),
+      ConstantArb (Number) (NumberArb),
+      ConstantArb (Number) (NumberArb),
+    ],
+  });
+});
+
+suite ('Semigroup laws', () => {
+  testLaws (laws.Semigroup (Z.equals)) ({
+    associativity: [
+      ConstantArb (String) (jsc.string),
+      ConstantArb (String) (jsc.string),
+      ConstantArb (String) (jsc.string),
+    ],
+  });
+});
+
+suite ('Functor laws', () => {
+  testLaws (laws.Functor (Z.equals)) ({
+    identity: [
+      ConstantArb (String) (jsc.string),
+    ],
+    composition: [
+      ConstantArb (String) (jsc.string),
+      jsc.constant (Math.sqrt),
+      jsc.constant (Math.abs),
+    ],
+  });
+});
+
+suite ('Bifunctor laws', () => {
+  testLaws (laws.Bifunctor (Z.equals)) ({
+    identity: [
+      ConstantArb (String) (jsc.string),
+    ],
+    composition: [
+      ConstantArb (String) (jsc.string),
+      jsc.constant (Math.sqrt),
+      jsc.constant (s => s.length),
+      jsc.constant (Math.sqrt),
+      jsc.constant (Math.abs),
+    ],
+  });
+});
+
+suite ('Apply laws', () => {
+  testLaws (laws.Apply (Z.equals)) ({
+    composition: [
+      ConstantArb (String) (jsc.string),
+      ConstantArb (String) (jsc.string),
+      ConstantArb (String) (jsc.string),
+    ],
+  });
+});
+
+suite ('Applicative laws', () => {
+  testLaws (laws.Applicative (Z.equals, Constant (Array))) ({
+    identity: [
+      ConstantArb (Array) (jsc.array (jsc.number)),
+    ],
+    homomorphism: [
+      jsc.constant (Math.abs),
+      jsc.number,
+    ],
+    interchange: [
+      ConstantArb (Array) (jsc.array (jsc.number)),
+      jsc.string,
+    ],
+  });
+});
+
+suite ('Foldable laws', () => {
+  testLaws (laws.Foldable (Z.equals)) ({
+    associativity: [
+      jsc.constant (s => x => s + s),
+      jsc.string,
+      ConstantArb (Number) (jsc.number),
+    ],
+  });
+});
+
+suite ('Traversable laws', () => {
+  testLaws (laws.Traversable (Z.equals)) ({
+    naturality: [
+      jsc.constant (Array),
+      jsc.constant (Identity),
+      jsc.constant (xs => Identity (xs[0])),
+      ConstantArb (Array) (NonEmpty (jsc.array (jsc.number))),
+    ],
+    identity: [
+      jsc.constant (Array),
+      ConstantArb (String) (jsc.string),
+    ],
+    composition: [
+      jsc.constant (Array),
+      jsc.constant (Identity),
+      ConstantArb (Array) (jsc.array (IdentityArb (jsc.string))),
+    ],
+  });
+});
