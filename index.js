139105069f825dc0e428ed3f22c09f8aa32cd876
David Chambers <dc@davidchambers.me>
2020-08-29 14:26:47 +0200

    remove ‘inspect’ fallback

--- index.js
+++ index.js
@@ -121,12 +121,10 @@
       /* eslint-enable key-spacing */
     };
 
-    var custom = util.inspect.custom;
+    var custom = util.inspect.custom;  // added in Node.js v6.6.0
     /* istanbul ignore else */
     if (typeof custom === 'symbol') {
       prototype[custom] = Constant$prototype$show;
-    } else {
-      prototype.inspect = Constant$prototype$show;
     }
 
     function Constant$bound(value) {
